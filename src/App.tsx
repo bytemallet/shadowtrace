12dc4d42bf7e0e9eae7395974716582653428680
bytemallet <articulosinfosec@gmail.com>
2025-08-21 19:50:29 +0200

    path fix

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/shadowtrace">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
